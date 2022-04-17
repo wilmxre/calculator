facfd26a605392327ee498bc3f89a9691ffdb6de
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-17 17:31:19 +0300

    Add labels to the buttons

--- script.js
+++ script.js
@@ -7,14 +7,36 @@ const operate = (operator, a, b) => operator(a, b);
 
 const calculator = document.querySelector('.calculator');
 const buttons = document.querySelector('.buttons');
-// const btn = document.querySelector('.btn');
+const span = document.querySelector('span');
 
+//create buttons
 for (let i = 0; i < 16; i++) {
   const btn = document.createElement('div');
   btn.classList.add('btn');
   buttons.appendChild(btn)
 }
 
-const addLabel = (elem, label) => {
+// add label to the buttons
+const addLabel = (elem, label, id) => {
   elem.textContent = label;
+  elem.setAttribute('id', id);
 }
+
+addLabel(buttons.childNodes[0], '7', 'seven');
+addLabel(buttons.childNodes[1], '8', 'eight');
+addLabel(buttons.childNodes[2], '9', 'nine');
+addLabel(buttons.childNodes[3], '\u00f7', 'divide');
+addLabel(buttons.childNodes[4], '4', 'four');
+addLabel(buttons.childNodes[5], '5', 'five');
+addLabel(buttons.childNodes[6], '6', 'six');
+addLabel(buttons.childNodes[7], '\u00D7', 'multiply');
+addLabel(buttons.childNodes[8], '1', 'one');
+addLabel(buttons.childNodes[9], '2', 'two');
+addLabel(buttons.childNodes[10], '3', 'three');
+addLabel(buttons.childNodes[11], '-', 'minus');
+addLabel(buttons.childNodes[12], '0', 'zero');
+addLabel(buttons.childNodes[13], '\u30FB', 'decimal');
+addLabel(buttons.childNodes[14], '=', 'equals');
+addLabel(buttons.childNodes[15], '+', 'plus');
+
+span.textContent = '123456';
