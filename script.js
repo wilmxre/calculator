3a2f9d9a30a190e73b3df4c47f9451eed18cbdd4
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-17 15:31:02 +0300

    Create basic layout

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 const operate = (operator, a, b) => operator(a, b);
+
+const calculator = document.querySelector('.calculator');
+const buttons = document.querySelector('.buttons');
+// const btn = document.querySelector('.btn');
+
+for (let i = 0; i < 25; i++) {
+  const btn = document.createElement('div');
+  btn.classList.add('btn');
+  buttons.appendChild(btn)
+}
