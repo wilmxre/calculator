fd021609539c53e80a48c65ad75f2baee490ae3d
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-20 18:05:40 +0300

    Fix major problem :)

--- script.js
+++ script.js
@@ -109,10 +109,11 @@ const concatInput = (elem) => {
       commands.push(command);
       equals.push(false);
 
-      console.log(equals[equals.length - 2], equals)
+      console.log(typeof equals[equals.length - 2], equals[equals.length - 2])
 
 
-      if (displayValue !== '' && prevValue !== 0) {
+      if (displayValue !== '' && prevValue !== 0 && equals[equals.length - 2] !== true) {
+        console.log('yo')
         displayValue = curValue = isFloat(operations(commands[commands.length - 2], prevValue, curValue));
         res.textContent = displayValue;
       }
