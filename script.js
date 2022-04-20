61767c6b63015a467236f4031514464e635359ac
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-20 22:24:49 +0300

    Add color effects and credits

--- script.js
+++ script.js
@@ -22,32 +22,36 @@ for (let i = 0; i < 16; i++) {
 }
 
 // add label to the buttons
-const addLabel = (elem, label, id) => {
+const addLabel = (elem, label, id, cls) => {
   elem.textContent = label;
   elem.setAttribute('id', id);
+
+  if (cls) {
+    elem.classList.add(cls);
+  }
 }
 
-addLabel(buttons.childNodes[0], '7', 'seven');
-addLabel(buttons.childNodes[1], '8', 'eight');
-addLabel(buttons.childNodes[2], '9', 'nine');
-addLabel(buttons.childNodes[3], '\u00f7', 'divide');
-addLabel(buttons.childNodes[4], '4', 'four');
-addLabel(buttons.childNodes[5], '5', 'five');
-addLabel(buttons.childNodes[6], '6', 'six');
-addLabel(buttons.childNodes[7], '\u00D7', 'multiply');
-addLabel(buttons.childNodes[8], '1', 'one');
-addLabel(buttons.childNodes[9], '2', 'two');
-addLabel(buttons.childNodes[10], '3', 'three');
-addLabel(buttons.childNodes[11], '-', 'subtract');
-addLabel(buttons.childNodes[12], '0', 'zero');
-addLabel(buttons.childNodes[13], '.', 'decimal');
-addLabel(buttons.childNodes[14], '+', 'add');
-addLabel(buttons.childNodes[15], '=', 'equals');
+addLabel(buttons.childNodes[0], '7', 'seven', 0);
+addLabel(buttons.childNodes[1], '8', 'eight', 0);
+addLabel(buttons.childNodes[2], '9', 'nine', 0);
+addLabel(buttons.childNodes[3], '\u00f7', 'divide', 'operator');
+addLabel(buttons.childNodes[4], '4', 'four', 0);
+addLabel(buttons.childNodes[5], '5', 'five', 0);
+addLabel(buttons.childNodes[6], '6', 'six', 0);
+addLabel(buttons.childNodes[7], '\u00D7', 'multiply', 'operator');
+addLabel(buttons.childNodes[8], '1', 'one', 0);
+addLabel(buttons.childNodes[9], '2', 'two', 0);
+addLabel(buttons.childNodes[10], '3', 'three', 0);
+addLabel(buttons.childNodes[11], '-', 'subtract', 'operator');
+addLabel(buttons.childNodes[12], '0', 'zero', 0);
+addLabel(buttons.childNodes[13], '.', 'decimal', 'operator');
+addLabel(buttons.childNodes[14], '+', 'add', 'operator');
+addLabel(buttons.childNodes[15], '=', 'equals', 'eq');
 
 // reset calculator to its original size
 resetDisplay = (size = 0) => {
-  calculator.style = `height: ${35 + size}rem`;
-  display.style = `height: ${5 + size}rem`;
+  calculator.style = `height: ${37 + size}rem`;
+  display.style = `height: ${7.4 + size}rem`;
 }
 
 // clear content of display
@@ -84,8 +88,8 @@ const isFloat = (number) => {
 const adjustDisplay = (size) => {
   if (size > 15) {
     let computedSize = parseInt(size / 16) * 2.7;
-    display.style = `height: ${5 + computedSize}rem;`;
-    calculator.style = `height: ${35 + computedSize}rem;`;
+    display.style = `height: ${7.4 + computedSize}rem;`;
+    calculator.style = `height: ${37 + computedSize}rem;`;
   }
 }
 
@@ -138,7 +142,7 @@ const concatInput = (elem) => {
 
     // if the button is the equals sign
     else if (e.target.id === 'equals') {
-      resetDisplay(2.4);
+      resetDisplay();
 
       equals.push(true);
       //  if the equals sign was pressed the first time, without any number input
